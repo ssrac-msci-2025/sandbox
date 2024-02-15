9ef5ed6b79785966e8f48ad225e1949180bcd9c0
cmhrabi <calum.hrabi@gmail.com>
2024-02-14 22:53:23 -0500

    Removed comments

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  // Initialise and set a string s
-  // to 'Hello World'
   s := "Hello World"
-
-  // Write out the string to the console
-  // Return the string for our later step.
   fmt.Println(messageOutput(s))
 }
 
